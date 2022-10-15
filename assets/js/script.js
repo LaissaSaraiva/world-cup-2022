ca1960a69dd59bed88d39aca823eb8930b9a053d
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-10-15 11:02:08 -0300

    Passo 01:Consumir de uma API as 32 seleções da Copa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-alert('olá, mundo');
+// Consumir de uma API as 32 seleções da Copa
+
+function getDados() {
+  const url = "https://estagio.geopostenergy.com/WorldCup/GetAllTeams";
+
+  let resposta = "";
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "git-user": "laissaSaraiva",
+    },
+  })
+    .then((response) => response.json())
+    .then((resposta) => {
+      console.log(resposta);
+    });
+}
+
+getDados();
