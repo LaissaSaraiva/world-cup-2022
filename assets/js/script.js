f3f93e8441b59a4cdab6d57af4af8d42c9099809
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-10-18 21:25:36 -0300

    Testando a Manipulação do Array
     -Removendo Objeto Token;
    
    Função para Divisão do Array;
    
    - Inclusão de propriedades ao Array;

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,19 +17,45 @@ function getDados() {
     .then((resposta) => {
       console.log(resposta);
       resultado = resposta.Result; //O Array a ser manipulado
-      // selecao = resultado[index].Name
       console.log(resultado); //Array
-
+      
       resultado.forEach(function (element, index) {
         let selecao = element.Name;
         console.log(index, selecao);
         index++;
+
+        //Removendo o objeto Token
+        for (let i = 0; i < resultado.length; i++) {
+          let aSerRemovido = resultado[i];
+          delete aSerRemovido.Token;
+        }
       });
+
+      //Função para Divisão de Array
+      let novoArray = [];
+      function dividirGrupos(array, equipesPorGrupo) {
+        let i = 0;
+        while (i < array.length) {
+          novoArray.push(array.slice(i, i + equipesPorGrupo));
+          i = i + equipesPorGrupo;
+        }
+        return novoArray;
+      }
+
+      const divisaoInicial = dividirGrupos(resultado, 4); // Array dos 8 times iniciais
+      console.log(divisaoInicial);
+
+      // Inclusão de propriedades ao ARRAY ( Nome do grupos/ pontuações e etc)
+      divisaoInicial.forEach(function (item, indice) {
+        item.unshift(`Grupo: ${indice}`); // Inserindo Nome do grupo na primeira posição de cada Array
+      });
+
+      console.log(divisaoInicial);
 
       // Loop para inclusão do texto no HTML
       for (let i = 0; i < resultado.length; i++) {
         let conteudo = document.createElement("p"); // Criando Paragrafo
-        conteudo.classList.add('selecao'); // Adicionando estilo ao parágrafo
+        conteudo.classList.add("selecao"); // Adicionando estilo ao parágrafo
         conteudo.innerHTML = `${i} - ${resultado[i].Name}`; // incluíndo conteúdo no html
         html.appendChild(conteudo); // adicionando tag <p> a uma <div>
       }
